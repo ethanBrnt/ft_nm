3670eec7a2e31a6a6ede141a0657fb0a43d63efe
etburnet <ethan.burnet.jaulmes@gmail.com>
2026-03-17 15:35:36 +0100

    doc et mise en place des idees

--- main.c
+++ main.c
@@ -6,52 +6,69 @@
 /*   By: eburnet <eburnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 11:51:06 by eburnet           #+#    #+#             */
-/*   Updated: 2026/03/13 12:10:41 by eburnet          ###   ########.fr       */
+/*   Updated: 2026/03/18 15:05:37 by eburnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-int
-main(int argc, char *argv[])
+#include <sys/mman.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
 {
-    struct stat sb;
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    if (stat(argv[1], &sb) == -1) {
-        perror("stat");
-        exit(EXIT_FAILURE);
-    }
-    printf("Type de fichier :                  ");
-    switch (sb.st_mode & S_IFMT) {
-    case S_IFBLK:  printf("périphérique bloc\n");       break;
-    case S_IFCHR:  printf("périphérique caractère\n");  break;
-    case S_IFDIR:  printf("répertoire\n");              break;
-    case S_IFIFO:  printf("FIFO/tube\n");               break;
-    case S_IFLNK:  printf("lien symbolique\n");         break;
-    case S_IFREG:  printf("fichier ordinaire\n");       break;
-    case S_IFSOCK: printf("socket\n");                  break;
-    default:       printf("inconnu ?\n");               break;
-    }
-    printf("Numéro d'inœud :                   %ld\n", (long) sb.st_ino);
-    printf("Mode :                             %lo (octal)\n",
-            (unsigned long) sb.st_mode);
-    printf("Nombre de liens :                  %ld\n", (long) sb.st_nlink);
-    printf("Propriétaires :                    UID=%ld   GID=%ld\n",
-            (long) sb.st_uid, (long) sb.st_gid);
-    printf("Taille de bloc d'E/S :             %ld octets\n",
-            (long) sb.st_blksize);
-    printf("Taille du fichier :                %lld octets\n",
-            (long long) sb.st_size);
-    printf("Blocs alloués :                    %lld\n",
-            (long long) sb.st_blocks);
-    printf("Dernier changement d'état :        %s", ctime(&sb.st_ctime));
-    printf("Dernier accès au fichier :         %s", ctime(&sb.st_atime));
-    printf("Dernière modification du fichier : %s", ctime(&sb.st_mtime));
-    exit(EXIT_SUCCESS);
+	int file_length = 0;
+	int fd_file;
+	char *filename = "a.out";
+
+	// check argc <= 2
+		// if (argc == 2)
+			// check argv[1] != null && argv[1] != ""
+			// char filename = argv[1]
+	// fd_file = open(filename);
+	// check if filename exist (open)
+	// check if filnemae is an ELF binary
+		/* read the first 16 bytes to check the file identity. 
+			The first four bytes will equal {0x7f, 'E', 'L', 'F'} */
+	// verifier que le fichier est nul terminer
+		// ??
+	/* fstat depuis un fd stock dans une struc ses infos technique 
+		(stock sa taille en bits) */
+	/* mmap stock le contenu d'un fichier en memoire en lui donnant le fd 
+		mais aussi la size contenue dans la struct de fstats */
+	// void *addr = mmap(NULL, file_length, PROT_READ, MAP_PRIVATE, fd_file, offset);
+		// offset multiple de page size (getpagesize())
+	/* Pour lire dans le fichier ELF il faut parcourir le Header
+		ELF puis acceder aux diferentes sections */
+	// QUESTION:
+		// de quoi est composer le header ?
+			/* composer en premier de e_ident (un tableau d'octet) qui permet de definir le 
+				type de fichier. Avec EI_MAG0-3 qui doivent avoir pour valeur 
+				nom dans le include    (ELFMAG0  ELFMAG1  ELFMAG2  ELFMAG3)
+				valeur dans le include ( 0x7f       E        L        F   )
+				et EI_CLASS qui definit l'archi system, valeur a ELFCLASS32 | ELFCLASS64 */
+			// il faut donc commencer par lire e_ident qui est toujour le meme, 
+			// verifier que c'est un ELF, et enregistrer sa class (32 ou 64)
+			/* Ensuite e_type ET_REL Un fichier repositionnable(.o), 
+				ET_EXEC Un fichier exécutable, ET_DYN Un objet partagé (.so). */
+		// comment parcourir les element du header ou autre dans mmap ?
+			// en castant le ptr a l'adresse choisi
+			// Elf64_Ehdr *header = (Elf64_Ehdr *)ptr;
+			// ou en ptr1 = (char *)ptr + e_shoff pour se deplacer a l'adresse de e_shoff
+			// puis cast du ptr1 dans la struc ou var approprier
+		
+		// nm affiche l'adresse memoire, le type, le nom du symbole ()
+			// un symbole est tout cd qui a un nom et une adresse memoire 
+				// (var global, fonction, var static)
+		// .strtab - symbol table, il faut donc lire dans la symbole table des fichier ELF
+		// comment acceder a la symbol table ?
+		/* Elf64_Ehdr -> e_shoff adresse pour acceder au tableau d'entete (header) DE SECTION,
+						 e_shnum nombre de section dans le tableau de header
+						 e_shentsize taille du header de la section qui est la meme pour tout les header
+		e_shentsize x e_shnum = taille en octets de la table des entêtes de sections
+		Il faut parcourir le tableau de Elf[64-32]_Shdr et trouve sh_type == SHT_SYMTAB 
+		sh_type est toujour le 2nd element du tableau
+		Si le sh_type == SHT_SYMTAB alors la section contient .symtab */
+		/* Elf[32-64]_Sym comporte les memes champs mais pas dans le meme ordre
+			st_name (nom du symbole), st_value (addr ?), st_info (type du symbole) */
+		// pour acceder au Elfn_Sym aller a (char)ptr + sh_offset de Elfn_Shdr
+		// sh_entsize donne le nombre de symbole present dans le tableau
 }
